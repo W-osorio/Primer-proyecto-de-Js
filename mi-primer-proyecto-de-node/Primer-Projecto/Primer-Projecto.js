54987122f432ada1250b82f653aba5ab6f10ce46
W-osorio <111244407+W-osorio@users.noreply.github.com>
2023-06-13 22:39:25 -0400

    Add files via upload

---
+++ mi-primer-proyecto-de-node/Primer-Projecto/Primer-Projecto.js
@@ -0,0 +1,14 @@
+const minombre= 'willimder'
+
+const miedad= 22
+
+const eresdesarollador= true
+
+const fecha= 'april 02 2001'
+
+const libro = {
+    titulo: "Piense y hagse  rico",
+    Autor: "Napoleon Hill",
+    fecha: 1937,
+    url: www.amazon.com/-/es/NAPOLEON-HILL/dp/8497778219, 
+}
